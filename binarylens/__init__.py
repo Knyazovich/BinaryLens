7364d942d60dce57a0e062294c7149c24516bad4
Knyaz <Vakaiii@proton.me>
2026-08-15 03:21:20 +0200

    Delete binarylens directory

--- binarylens/__init__.py
+++
@@ -1,3 +0,0 @@
-"""BinaryLens - Static Binary Analysis Tool."""
-
-__version__ = "1.0.0"
--- binarylens/analysis/indicators/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Detection engine rule modules.
-
-Each module here implements one family of correlation rules and returns
-Finding objects. `binarylens.analysis.correlation` orchestrates all of
-them into the final capabilities/findings on an AnalysisResult.
-"""
